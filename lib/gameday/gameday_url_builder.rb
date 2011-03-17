4c63d3d8f9eef5768294a2f6826da2b6769be5e2
bbennett <bbennett@attinteractive.com>
2011-03-17 08:18:01 -0700

    Small cleanup of url builder.

--- lib/gameday/gameday_url_builder.rb
+++ lib/gameday/gameday_url_builder.rb
@@ -1,120 +1,105 @@
 module Gameday
   class GamedayUrlBuilder
-      def self.build_game_base_url(gid)
-      gameday_info = GamedayUtil.parse_gameday_id('gid_' + gid)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + gameday_info['year'] + "/month_" + gameday_info['month'] + "/day_" + gameday_info['day'] + "/gid_"+gid 
+    def self.build_game_base_url(gid)
+      gid_info = GamedayUtil.parse_gameday_id('gid_' + gid)
+      y, m, d = gid_info['year'], gid_info['month'], gid_info['day']
+
+      "#{build_day_url(y, m, d)}/gid_#{gid}"
     end
-  
-  
+
+
     def self.build_eventlog_url(year, month, day, gid)
-      set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/eventLog.xml" 
+      "#{game_url(year, month, day, gid)}/eventLog.xml"
     end
-  
-  
+
+
     def self.build_epg_url(year, month, day)
-      set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/epg.xml"
+      "#{build_day_url(year, month, day)}/epg.xml"
     end
-  
-  
+
+
     def self.build_scoreboard_url(year, month, day)
-      set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/master_scoreboard.xml"
+      "#{build_day_url(year, month, day)}/master_scoreboard.xml"
     end
-  
-  
+
+
     def self.build_scoreboard_json_url(year, month, day)
       "#{build_day_url(year, month, day)}/master_scoreboard.json"
     end
-  
-  
+
+
     def self.build_day_highlights_url(year, month, day)
-      set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/media/highlights.xml"
+      "#{build_day_url(year, month, day)}/media/highlights.xml"
     end
-  
-  
+
+
     def self.build_boxscore_url(year, month, day, gid)
-      set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/boxscore.xml" 
+      "#{game_url(year, month, day, gid)}/boxscore.xml"
     end
-  
-  
+
+
     def self.build_game_url(year, month, day, gid)
-      set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/game.xml" 
+      "#{game_url(year, month, day, gid)}/game.xml"
     end
-  
-  
+
+
     def self.build_game_events_url(year, month, day, gid)
-      set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/game_events.xml" 
+      "#{game_url(year, month, day, gid)}/game_events.xml"
     end
-  
-  
+
+
     def self.build_gamecenter_url(year, month, day, gid)
-      set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/gamecenter.xml" 
+      "#{game_url(year, month, day, gid)}/gamecenter.xml"
     end
 
 
     def self.build_linescore_url(year, month, day, gid)
-      set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/linescore.xml" 
+      "#{game_url(year, month, day, gid)}/linescore.xml"
     end
-  
+
 
     def self.build_players_url(year, month, day, gid)
-      set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/players.xml" 
+      "#{game_url(year, month, day, gid)}/players.xml"
     end
-  
-  
+
+
     def self.build_batter_url(year, month, day, gid, pid)
-      set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/batters/" +  pid + '.xml'
+      "#{game_url(year, month, day, gid)}/batters/#{pid}.xml"
     end
-  
-  
+
+
     def self.build_pitcher_url(year, month, day, gid, pid)
-      set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/pitchers/" +  pid + '.xml' 
+      "#{game_url(year, month, day, gid)}/pitchers/#{pid}.xml"
     end
-  
-  
+
+
     def self.build_inningx_url(year, month, day, gid, inning_num)
-      set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/inning/inning_#{inning_num}.xml"
+      "#{game_url(year, month, day, gid)}/inning/inning_#{inning_num}.xml"
     end
-  
-  
+
+
     def self.build_inning_scores_url(year, month, day, gid)
-      set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/inning/inning_Scores.xml"
+      "#{game_url(year, month, day, gid)}/inning/inning_Scores.xml"
     end
-  
-  
+
+
     def self.build_inning_hit_url(year, month, day, gid)
-      set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/inning/inning_hit.xml"
+      "#{game_url(year, month, day, gid)}/inning/inning_hit.xml"
     end
-  
+
 
     def self.build_day_url(year, month, day)
       set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_#{@@year}/month_#{@@month}/day_#{@@day}/"
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_#{@@year}/month_#{@@month}/day_#{@@day}"
     end
-  
-  
-    def self.build_month_url(year, month)
-      set_date_vars(year, month, nil)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_#{@@year}/month_#{@@month}/"
-    end
-  
-  
+
+
     private
-  
+
+    def self.game_url year, month, day, gid
+      "#{build_day_url(year, month, day)}/gid_#{gid}"
+    end
+
     def self.set_date_vars(year, month, day)
       @@year = GamedayUtil.convert_digit_to_string(year.to_i)
       @@month = GamedayUtil.convert_digit_to_string(month.to_i)
@@ -122,7 +107,5 @@ module Gameday
         @@day = GamedayUtil.convert_digit_to_string(day.to_i)
       end
     end
-  
-  
   end
 end
--- test/unit_tests/test_gameday_url_builder.rb
+++ test/unit_tests/test_gameday_url_builder.rb
@@ -4,102 +4,96 @@ require 'test/unit'
 require 'gameday_url_builder'
 
 class TestGamedayUrlBuilder < Test::Unit::TestCase
-  
-  
-  def setup
-
-  end
-  
 
   def test_build_eventlog_url
     url = Gameday::GamedayUrlBuilder.build_eventlog_url('2009', '9', '20', '2009_09_20_detmlb_minmlb_1')
     assert_not_nil url
     assert_equal "http://gd2.mlb.com/components/game/mlb/year_2009/month_09/day_20/gid_2009_09_20_detmlb_minmlb_1/eventLog.xml" , url
   end
-  
-  
+
+
   def test_build_scoreboard_url
     url = Gameday::GamedayUrlBuilder.build_scoreboard_url('2009', '9', '20')
     assert_not_nil url
     assert_equal 'http://gd2.mlb.com/components/game/mlb/year_2009/month_09/day_20/master_scoreboard.xml' , url
   end
-  
-  
+
+
   def test_build_boxscore_url
     url = Gameday::GamedayUrlBuilder.build_boxscore_url('2009', '9', '20', '2009_09_20_detmlb_minmlb_1')
     assert_not_nil url
     assert_equal "http://gd2.mlb.com/components/game/mlb/year_2009/month_09/day_20/gid_2009_09_20_detmlb_minmlb_1/boxscore.xml" , url
   end
-  
-  
+
+
   def test_build_game_url
     url = Gameday::GamedayUrlBuilder.build_game_url('2009', '9', '20', '2009_09_20_detmlb_minmlb_1')
     assert_not_nil url
     assert_equal "http://gd2.mlb.com/components/game/mlb/year_2009/month_09/day_20/gid_2009_09_20_detmlb_minmlb_1/game.xml" , url
   end
-  
-  
+
+
   def test_build_gamecenter_url
     url = Gameday::GamedayUrlBuilder.build_gamecenter_url('2009', '9', '20', '2009_09_20_detmlb_minmlb_1')
     assert_not_nil url
     assert_equal "http://gd2.mlb.com/components/game/mlb/year_2009/month_09/day_20/gid_2009_09_20_detmlb_minmlb_1/gamecenter.xml" , url
   end
-  
-  
+
+
   def test_build_linescore_url
     url = Gameday::GamedayUrlBuilder.build_linescore_url('2009', '9', '20', '2009_09_20_detmlb_minmlb_1')
     assert_not_nil url
     assert_equal "http://gd2.mlb.com/components/game/mlb/year_2009/month_09/day_20/gid_2009_09_20_detmlb_minmlb_1/linescore.xml" , url
   end
-  
-  
+
+
   def test_build_players_url
     url = Gameday::GamedayUrlBuilder.build_players_url('2009', '9', '20', '2009_09_20_detmlb_minmlb_1')
     assert_not_nil url
     assert_equal "http://gd2.mlb.com/components/game/mlb/year_2009/month_09/day_20/gid_2009_09_20_detmlb_minmlb_1/players.xml" , url
   end
-  
-  
+
+
   def test_build_batter_url
     url = Gameday::GamedayUrlBuilder.build_batter_url('2009', '9', '20', '2009_09_20_detmlb_minmlb_1','12345')
     assert_not_nil url
     assert_equal "http://gd2.mlb.com/components/game/mlb/year_2009/month_09/day_20/gid_2009_09_20_detmlb_minmlb_1/batters/12345.xml" , url
   end
-  
-  
+
+
   def test_build_pitcher_url
     url = Gameday::GamedayUrlBuilder.build_pitcher_url('2009', '9', '20', '2009_09_20_detmlb_minmlb_1','12345')
     assert_not_nil url
     assert_equal "http://gd2.mlb.com/components/game/mlb/year_2009/month_09/day_20/gid_2009_09_20_detmlb_minmlb_1/pitchers/12345.xml" , url
   end
-  
-  
+
+
   def test_build_pbp_inningx_url
     url = Gameday::GamedayUrlBuilder.build_inningx_url('2009', '9', '20', '2009_09_20_detmlb_minmlb_1','3')
     assert_not_nil url
     assert_equal "http://gd2.mlb.com/components/game/mlb/year_2009/month_09/day_20/gid_2009_09_20_detmlb_minmlb_1/inning/inning_3.xml" , url
   end
-  
-  
+
+
   def test_build_pbp_inning_scores_url
     url = Gameday::GamedayUrlBuilder.build_inning_scores_url('2009', '9', '20', '2009_09_20_detmlb_minmlb_1')
     assert_not_nil url
     assert_equal "http://gd2.mlb.com/components/game/mlb/year_2009/month_09/day_20/gid_2009_09_20_detmlb_minmlb_1/inning/inning_Scores.xml" , url
   end
-  
-  
+
+
   def test_build_pbp_inning_hit_url
     url = Gameday::GamedayUrlBuilder.build_inning_hit_url('2009', '9', '20', '2009_09_20_detmlb_minmlb_1')
     assert_not_nil url
     assert_equal "http://gd2.mlb.com/components/game/mlb/year_2009/month_09/day_20/gid_2009_09_20_detmlb_minmlb_1/inning/inning_hit.xml" , url
   end
-  
-  
+
+
   def test_build_day_url
     url = Gameday::GamedayUrlBuilder.build_day_url('2009', '9', '20')
     assert_not_nil url
-    assert_equal "http://gd2.mlb.com/components/game/mlb/year_2009/month_09/day_20/" , url
+    assert_equal "http://gd2.mlb.com/components/game/mlb/year_2009/month_09/day_20" , url
   end
-  
-  
+
+
 end
