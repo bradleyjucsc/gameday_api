5643cdefd94137ef4f63c043b64fb4632ed02d28
bbennett <bbennett@attinteractive.com>
2011-03-19 19:42:08 -0700

    Added raw_attrs to resource

--- lib/gameday/resource.rb
+++ lib/gameday/resource.rb
@@ -1,8 +1,11 @@
 module Gameday
   class Resource
+    attr_accessor :raw_attrs
 
     def self.new_from_hash hash
       res = self.new
+      res.raw_attrs = hash
+
       hash.each do |k,v|
         meth = "#{k}=".to_sym
         res.send(meth, v) if res.respond_to? meth
@@ -12,7 +15,7 @@ module Gameday
     end
 
     def self.new_from_xml element
-      return unless element.try(:attributes) === Hash
+      return unless element && element.attributes === Hash
 
       new_from_hash element.attributes
     end
---
+++ test/unit_tests/test_resource.rb
@@ -0,0 +1,25 @@
+$: << File.expand_path(File.dirname(__FILE__) + "/../../lib")
+
+require 'test/unit'
+require 'gameday'
+
+class TestResource < MiniTest::Unit::TestCase
+  class FakeResource < Gameday::Resource
+    attr_accessor :a, :b
+  end
+
+  def setup
+    @hash = {
+      :a => 'eh',
+      :b => 'bee',
+      :c => 'see'
+    }
+  end
+
+  def test_new_from_hash
+    r = FakeResource.new_from_hash @hash
+    assert_equal @hash,     r.raw_attrs
+    assert_equal @hash[:a], r.a
+    assert_equal @hash[:b], r.b
+  end
+end
